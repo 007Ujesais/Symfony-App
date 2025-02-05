3d62e362d6cf2b358ada2da9c189efd14248df46
ToojoNariv <tojomanambina@gmail.com>
2025-02-05 20:42:26 +0300

    cccc

---
+++ assets/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1>Bienvenue sur mon application Vue.js avec Symfony !</h1>
+      <p>Ceci est un composant Vue rendu dans Symfony.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: "App"
+  };
+  </script>
+  
+  <style scoped>
+  h1 {
+    color: #42b983;
+  }
+  </style>
+  
