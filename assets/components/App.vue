b2cff41bb4842fef160044ca18f920c9d143070b
ToojoNariv <tojomanambina@gmail.com>
2025-02-06 06:18:42 +0300

    vite

--- assets/components/App.vue
+++ assets/components/App.vue
@@ -1,16 +1,15 @@
 <template>
     <div>
       <h1>Bienvenue sur mon application Vue.js avec Symfony !</h1>
-      <p>Ceci est un composant Vue rendu dans Symfony.</p>
     </div>
   </template>
   
-  <!-- <script>
+  <script>
   export default 
   {
     name: 'App'
   };
-  </script> -->
+  </script>
   
   <style scoped>
   h1 {
